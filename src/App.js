5b763b474f07cb69c59e4b798b905496051670b9
Peter Tyldesley <ptylde@gmail.com>
2017-07-11 16:01:23 +0100

    Add basic intro

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="jumbotron">
-  <h1>Hello, world!</h1>
-  <p>...</p>
-  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-</div>
+        <div className="jumbotron">
+          <div className="intro container">
+          <div className="row">
+            <div className="col-md-offset-3 col-md-6 col-sm-12">
+              <h2><strong>Peter Tyldesley</strong></h2>
+              <h2 className="sans"><span className="purple">Full-stack</span> <span className="pink">software engineer</span><br/>
+              </h2>
+              <hr/>
+              <blockquote>I use the <span className="highlight">entire</span> tech stack to create <span className="highlight">products</span> that people <span className="highlight">love</span> to use.</blockquote>
+              <br/>
+              <h5>Find out more...</h5>
+            </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
