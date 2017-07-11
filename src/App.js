d66dd737636499f212a942b577dbe368f0a5e4cd
Peter Tyldesley <ptylde@gmail.com>
2017-07-11 16:55:48 +0100

    Add Intro component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Intro from './components/intro';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="jumbotron">
-          <div className="intro container">
-          <div className="row">
-            <div className="col-md-offset-3 col-md-6 col-sm-12">
-              <h2><strong>Peter Tyldesley</strong></h2>
-              <h2 className="sans"><span className="purple">Full-stack</span> <span className="pink">software engineer</span><br/>
-              </h2>
-              <hr/>
-              <blockquote>I use the <span className="highlight">entire</span> tech stack to create <span className="highlight">products</span> that people <span className="highlight">love</span> to use.</blockquote>
-              <br/>
-              <h5>Find out more</h5>
-            </div>
-            </div>
-          </div>
-        </div>
+        <Intro />
       </div>
     );
   }
---
+++ src/components/intro.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Ionicon from 'react-ionicons';
+
+export default class Intro extends Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <div className="intro container">
+          <div className="row">
+            <div className="col-md-offset-3 col-md-6 col-sm-12">
+              <h2><strong>Peter Tyldesley</strong></h2>
+                <h2 className="sans"><span className="purple">Full-stack</span> <span className="pink">software engineer</span><br/>
+              </h2>
+              <hr/>
+              <blockquote>I use the <span className="highlight">entire</span> tech stack to <span className="highlight">create</span> products that people <span className="highlight">love</span> to use.</blockquote>
+              <br/>
+              <div className="text-center more">
+                <h5>Find out more</h5>
+                <Ionicon icon="ion-ios-arrow-down" fontSize="25px" color="white"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
