58baf17e25118c6bd9e8b656db78ec13296280fb
Peter Tyldesley <ptylde@gmail.com>
2017-07-11 17:39:21 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Intro from './components/intro';
+import FixedNavbar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Intro />
+        <FixedNavbar />
       </div>
     );
   }
---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import Ionicon from 'react-ionicons';
+
+export default class FixedNavbar extends Component {
+  render() {
+    return (
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">About</NavItem>
+            <NavItem eventKey={2} href="#">Portfolio</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">ptylde@gmail.com</NavItem>
+            <NavItem eventKey={2} href="#"><Ionicon icon="ion-social-octocat" color="#7DF9FF"/></NavItem>
+            <NavItem eventKey={2} href="#"><Ionicon icon="ion-social-instagram" color="#7DF9FF"/></NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
