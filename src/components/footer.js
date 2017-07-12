5ead466bd7fc1f52ddbc0d8c7ef29502fbc3abfd
Peter Tyldesley <ptylde@gmail.com>
2017-07-12 16:20:48 +0100

    Install react-tooltip

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="container-fluid text-center footer mono">
-      <h3>Thanks for dropping in!</h3>
+      <h3>Thanks for dropping in, fellow humanoid!</h3>
       <h3>ᕕ( ᐛ )ᕗ</h3>
       <br/>
       <p className="mono">Site by <span className="gray-underline">p12y</span> | &copy; 2017</p>
