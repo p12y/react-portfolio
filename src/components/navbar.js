0c801263136d226b85d922e3d02bba05056fd594
Peter Tyldesley <ptylde@gmail.com>
2017-07-13 18:19:33 +0100

    Add scroll-to to navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
 import Ionicon from 'react-ionicons';
 import MotionMenu from 'react-motion-menu';
+import scrollToElement from 'scroll-to-element';
 
 export default class FixedNavbar extends Component {
+  handleArrowClick(el) {
+    scrollToElement(el);
+  }
+
   render() {
     return (
       <div id="menu">
@@ -13,13 +18,13 @@ export default class FixedNavbar extends Component {
         <div className="btn btn-black">
           <Ionicon icon="ion-navicon" fontSize="32px" color="white"/>
         </div>
-        <div className="btn btn-black">
+        <div onClick={() => this.handleArrowClick('#about')} className="btn btn-black">
           <Ionicon icon="ion-person" fontSize="32px" color="white"/>
         </div>
-        <div className="btn btn-black">
+        <div onClick={() => this.handleArrowClick('#portfolio')} className="btn btn-black">
           <Ionicon icon="ion-code-working" fontSize="32px" color="white"/>
         </div>
-        <div className="btn btn-black">
+        <div onClick={() => this.handleArrowClick('#contact')} className="btn btn-black">
           <Ionicon icon="ion-paper-airplane" fontSize="32px" color="white"/>
         </div>
         </MotionMenu>
