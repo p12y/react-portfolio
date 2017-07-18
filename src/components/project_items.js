710716e280cf7cea188471b473819ee68c9bb22b
Peter Tyldesley <ptylde@gmail.com>
2017-07-18 21:33:19 +0100

    Remove backslash from img url

--- src/components/project_items.js
+++ src/components/project_items.js
@@ -6,9 +6,9 @@ export default class ProjectItems extends Component {
   render() {
     const projectItems = [
       {title: "How's the Weather", description: "is an Angular app that displays the weather in the user's current location.",
-      img_url: '/img/weather_app.png', url: 'https://codepen.io/thegreaterpanda/full/BWwJwE/'},
+      img_url: 'img/weather_app.png', url: 'https://codepen.io/thegreaterpanda/full/BWwJwE/'},
       {title: 'Joke Generator', description: 'is an app built with jQuery that displays random jokes from a JSON API.',
-      img_url: '/img/joke_generator.png', url: 'https://codepen.io/thegreaterpanda/full/ZBeNPe/'},
+      img_url: 'img/joke_generator.png', url: 'https://codepen.io/thegreaterpanda/full/ZBeNPe/'},
       {title: 'Wikipedia Viewer', description: 'is an Angular app written in HAML to allow easy viewing/searching of Wikipedia articles.',
       img_url: 'img/wikipedia_viewer.png', url: 'https://codepen.io/thegreaterpanda/full/JWOOpj/'},
       {title: 'YouTube Viewer', description: ['is a React app built with Semantic-UI to search for and view YouTube videos. ', 
@@ -18,7 +18,7 @@ export default class ProjectItems extends Component {
       {title: 'My Personal Portfolio', description: ['is a React app. Inception! ', 
       <br/>,
       <a href="https://github.com/p12y/portfolio/tree/master" target="_blank" rel="noopener noreferrer"><Ionicon icon="ion-social-github" color="white"/></a>],
-      img_url: '/img/portfolio.png', url: 'https://github.com/p12y/portfolio/tree/master'},
+      img_url: 'img/portfolio.png', url: 'https://github.com/p12y/portfolio/tree/master'},
       {title: 'Weather Compare', description: ['is a React app with state management by Redux, to compare the weather in different cities. ', 
       <br/>,
       <a href="https://github.com/p12y/weather_compare" target="_blank" rel="noopener noreferrer"><Ionicon icon="ion-social-github" color="white"/></a>],
