05cf29a87b44706123dd847466a8abc5e9f22402
Peter Tyldesley <ptylde@gmail.com>
2018-01-06 18:34:48 +0000

    Add new portfolio items

--- src/components/project_items.js
+++ src/components/project_items.js
@@ -5,24 +5,30 @@ import Ionicon from 'react-ionicons';
 export default class ProjectItems extends Component {
   render() {
     const projectItems = [
+      {title: "Shrnk", description: "is a URL shortener app and API built with Expresss.",
+      img_url: 'img/shrnk.png', url: 'https://shrnk.glitch.me/'},
+      {title: "Calculator", description: "is a React app.",
+      img_url: 'img/calculator.png', url: 'http://react-js-calculator.surge.sh'},
+      {title: "Markdown Previewer", description: "is a React app to preview markdown in the browser.",
+      img_url: 'img/markdown_previewer.png', url: 'http://p12y-md-previewer.surge.sh'},
+      {title: "Tic-Tac-Toe", description: "is a React app featuring that classic game.",
+      img_url: 'img/ttt.png', url: 'http://ttt-react.surge.sh'},
+      {title: "Pomodoro", description: "is a Pomodoro Timer built with React.",
+      img_url: 'img/pomodoro.png', url: 'http://react-pomodoro.surge.sh'},
+      {title: "Simon Game", description: "is a memory game built with React.",
+      img_url: 'img/simon_game.png', url: 'http://react-simon-game.surge.sh'},
+      {title: "Twitch Streamer", description: "is a React app that shows which shows are currently streaming on Twitch.tv.",
+      img_url: 'img/twitch.png', url: 'http://twitch-streamer.surge.sh/'},
       {title: "How's the Weather", description: "is an Angular app that displays the weather in the user's current location.",
       img_url: 'img/weather_app.png', url: 'https://codepen.io/thegreaterpanda/full/BWwJwE/'},
-      {title: 'Joke Generator', description: 'is an app built with jQuery that displays random jokes from a JSON API.',
-      img_url: 'img/joke_generator.png', url: 'https://codepen.io/thegreaterpanda/full/ZBeNPe/'},
       {title: 'Wikipedia Viewer', description: 'is an Angular app written in HAML to allow easy viewing/searching of Wikipedia articles.',
       img_url: 'img/wikipedia_viewer.png', url: 'https://codepen.io/thegreaterpanda/full/JWOOpj/'},
-      {title: 'YouTube Viewer', description: ['is a React app built with Semantic-UI to search for and view YouTube videos. ', 
-      <br/>,
-      <a href="https://github.com/p12y/youtube_viewer" target="_blank" rel="noopener noreferrer"><Ionicon icon="ion-social-github" color="white"/></a>],
+      {title: 'YouTube Viewer', description: 'is a React app built with Semantic-UI to search for and view YouTube videos. ',
       img_url: 'img/youtube_viewer.png', url: 'http://p12y.github.io/youtube_viewer'},
-      {title: 'My Personal Portfolio', description: ['is a React app. Inception! ', 
-      <br/>,
-      <a href="https://github.com/p12y/portfolio/tree/master" target="_blank" rel="noopener noreferrer"><Ionicon icon="ion-social-github" color="white"/></a>],
+      {title: 'My Personal Portfolio', description: 'is a React app. Inception!',
       img_url: 'img/portfolio.png', url: 'https://github.com/p12y/portfolio/tree/master'},
-      {title: 'Weather Compare', description: ['is a React app with state management by Redux, to compare the weather in different cities. ', 
-      <br/>,
-      <a href="https://github.com/p12y/weather_compare" target="_blank" rel="noopener noreferrer"><Ionicon icon="ion-social-github" color="white"/></a>],
-      img_url: 'img/weather_compare.png', url: 'http://p12y.github.io/weather_compare'},
+      {title: 'Weather Compare', description: 'is a React app with state management by Redux, to compare the weather in different cities.',
+      img_url: 'img/weather_compare.png', url: 'http://p12y.github.io/weather_compare'}
     ]
 
     const items = projectItems.map(item => {
