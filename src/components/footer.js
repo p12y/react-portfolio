12689ae85c9693357b29011a1959bf4d5b75fd8b
Peter Tyldesley <ptylde@gmail.com>
2018-01-06 17:36:59 +0000

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="container-fluid text-center footer mono">
-      <h3>Thanks for dropping in, fellow humanoid!</h3>
-      <h3>ᕕ( ᐛ )ᕗ</h3>
+      <h3>Thanks for dropping in!</h3>
+      <h3>ツ</h3>
       <br/>
       <p className="mono">Site by <span data-tip="I mean, really? What did you expect?" className="gray-underline"><a href="https://github.com/p12y" target="_blank" rel="noopener noreferrer">p12y</a></span> | &copy; 2017</p>
     </div>
